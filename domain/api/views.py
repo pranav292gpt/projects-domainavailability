6eae37b5d668248515eadc3dc57c3146b42c76e3
Po <pranav292gpt@gmail.com>
2017-05-08 15:23:04 +0530

    Basic working API

--- domain/api/views.py
+++ domain/api/views.py
@@ -1,6 +1,34 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import pythonwhois
 
-# Create your views here.
+class DomainChecker(APIView):
+    def get(self, request, *args, **kwargs):
+        if "domain" in request.GET:
+            domain = request.GET.get("domain")
+
+            try:
+                query = pythonwhois.get_whois(domain)
+            
+
+                if not query.get("id"):
+                    response = {
+                            "domain" : domain,
+                            "available" : True
+                            }
+                    return Response(response)
+                
+                response = {
+                        "domain" : domain,
+                        "creation_date" : str(query.get("creation_date")[0]),
+                        "expiration_date" : str(query.get("expiration_date")[0]),
+                        "updated_date" : str(query.get("updated_date")[0]),
+                        "available" : False
+                        }
+                return Response(response)
+            except Exception:
+                return Response({"error" : " No root WHOIS server found for domain."}, status.HTTP_400_BAD_REQUEST)
+        return Response({"error" : "bad request"}, status.HTTP_400_BAD_REQUEST)
